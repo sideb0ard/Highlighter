29144573ba815fdf6ab4e8e46e9d088ce738faad
Thorsten Sideb0ard <thorsten.sideb0ard@gmail.com>
2012-03-22 17:31:24 -0700

    heuristically hupped

--- sbz_highlighter.py
+++ sbz_highlighter.py
@@ -1,6 +1,31 @@
+# -*- coding: utf-8 -*-
 # Thorsten Sideboard's Flying Circus text highlighting guff.
 import re
 import sys
+
+#def compute_prefix(list):
+#    size = len(list)
+#    k = 0
+#    q = 0
+#    result = {}
+#    while q < size:
+      
+      
+
+def prepare_badcharacter_heuristic(query_list):
+    result = {}
+    for index,item in enumerate(query_list):
+      print index,item
+      result[item] = index
+    return result
+
+def prepare_goodsuffix_heuristic(query_list):
+    size = len(query_list)
+    result = {}
+
+    reverse = query_list[::-1]
+    print reverse
+
 
 def highlight_doc(doc,query):    # 
     doc_length = len(doc)
@@ -13,27 +38,36 @@ def highlight_doc(doc,query):    #
     doc_list = list(doc)
     query_list = list(query)
 
-    print doc_list[0:3]
+    #print doc_list[0:3]
 
-    s = 0
+    badcharacter = prepare_badcharacter_heuristic(query_list)
+    goodsuffix = prepare_goodsuffix_heuristic(query_list)
+    #print "BADBOTS", badcharacter
+    #print "GOODGUYS", goodsuffix
+
+    m = 0
+    s = 0 # index - position within Doc
     while  s < doc_length - query_length:
-      print "boom! ", s
-      s += 1
-
-   
+      j = query_length # ie. last character of query
+      print "Run ", s, "looking for ", query_list[j-1]
+      print "Looking at ", doc_list[s+j-1], " for ", query_list[j-1]
     
-#    print "LENGTHZ::"
-#    print doc_length, query_length
-#
-#    m = re.search(query, doc)
-#    split_query = re.split('\W+', query)
-#    #(m is not None) and (print "m.group(0)")
-#    if m is not None:
-#        print m.group(0)
-#
-#    print len(split_query)
-#    print split_query
-#    for word in split_query: print word,
+      while ((j > 0) and (query_list[j-1] == doc_list[s+j-1])):
+        print "MATCH FOUND! -- ", query_list[j-1], doc_list[s+j-1]
+
+        j -= 1 # now, with one match, lets move the char position postion within query to the left by one place
+        print "NOw looking for ", query_list[j-1]
+
+        if (j > 0): # i.e. not the first character
+          k = badcharacter.get(doc_list[s+j-1])
+          print "Moved k back one ", k
+        #  k = -1 unless k
+        #  if (k < j) and ((j-k-1) > goodsuffix[j]):
+        #    s += (j-k-1)
+        #  else
+        #    s+= goodsuffix[j];
+
+      s += 1
 
 
 if __name__ == "__main__":
