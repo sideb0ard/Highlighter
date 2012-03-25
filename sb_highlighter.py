5054ff6a14c5d4861d78460ff8866375275681a3
Thorsten Sideb0ard <Thorsten.Sideb0ard@gmail.com>
2012-03-24 22:48:52 -0700

    brain. melted

--- sb_highlighter.py
+++ sb_highlighter.py
@@ -3,19 +3,54 @@
 import sys
 import re
 
+
 def highlight_doc(doc,query):
+    query_words = query.split()
+    query_word_count = len(query_words) 
+    print "QUERY_WORD_COUNT {0}".format(query_word_count)
+    #if len(query_words) > 1:
+    #    for word in query_words:
+    #        print "WORD:: {0}".format(word)
+    #        single_word_highlight(displaytext,word)
+
+    # FIRST RUN FOR FULL QUERY
     result = re.search(query, doc)
+
     if result is None:
-        print "Sorry bud, no dice"
+        if query_word_count > 1:
+            print "SINGLE WORD SEARCH"
+            displaytext = single_word_highlight(doc,query_words)
+            print "\n\n{0}\n\n".format(displaytext)
+        else:
+            print "Sorry bud, no dice"
     else:
-        highlightOn = "[[HIGHLIGHT]]"
-        highlightOff = "[[ENDHIGHLIGHT]]"
         #print "RESULT! {0}".format(result)
         #displaytext = re.sub('(.*)(%s)(.*)' % query, '\\1%s\\2%s\\3' % (highlightOn,highlightOff), doc)
         #displaytext = re.sub(".*(.{,70})(%s)(.*)" % query, '\\1%s\\2%s\\3' % (highlightOn,highlightOff), doc)
-        displaytext = re.sub(".*?(.{,20})(%s)(.{,100})" % query, '\\1%s\\2%s\\3' % (highlightOn,highlightOff), doc)
+        displaytext = re.sub(".*?(.{,150})(%s)(.{,350})" % query, "\\1%s\\2%s\\3" % (highlightOn,highlightOff), doc, count=1)
+        displaytext = displaytext[:400]
+
         print "\n\n{0}\n\n".format(displaytext)
 
+
+def single_word_highlight(text,query_words):
+    wordtransform = text
+    x = 0
+    for word in query_words:
+        wordresult = re.search(word,wordtransform)
+        if wordresult is not None:
+            print "FOUND WORD {0}".format(word)
+            #print "WORDRESULT = {0}".format(wordtransform)
+            #wordtransform = re.sub("(.{300})(%s)(.*)" % word, "\\1%s\\2%s\\3" % (highlightOn,highlightOff), wordtransform, count=12)
+            if x < 1:
+                print "X LESS THAN ONE"
+                wordtransform = re.sub(".*?(.{,150})(%s)(.{,250})" % word, "\\1%s\\2%s\\3" % (highlightOn,highlightOff), wordtransform, count=12)
+                wordtransform = wordtransform[:600]
+            else:
+                wordtransform = re.sub("(.*)(%s)(.*)" % word, "\\1%s\\2%s\\3" % (highlightOn,highlightOff), wordtransform, count=12)
+            print "\n\nWORDDD{0}\n\n".format(wordtransform)
+        x += 1
+    return wordtransform
 
 if __name__ == "__main__":
 
@@ -34,6 +69,8 @@ if __name__ == "__main__":
       print "bzzt - does not compute - query should be longer than document"
       exit(1)
 
+    highlightOn = "[[HIGHLIGHT]]"
+    highlightOff = "[[ENDHIGHLIGHT]]"
     highlight_doc(doc,query)
 
     #pat = re.compile(r'([A-Z][^\.!?]*[\.!?])', re.M)
